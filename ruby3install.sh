0007722cce3197d53a2b1646db037abb6612a9d3
Nick Butler <nwmbutler@gmail.com>
2021-07-15 15:46:26 +0100

    chore: removed extraneous files

--- ruby3install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-apt install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
-curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash -
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-rbenv install -l
-rbenv install 3.0.0
-rbenv global 3.0.0
