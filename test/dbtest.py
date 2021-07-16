5f0de3cfccece919537178cc862df68e18bce12c
Nick Butler <nwmbutler@gmail.com>
2021-07-16 17:20:51 +0100

    feat: return tables

--- test/dbtest.py
+++ test/dbtest.py
@@ -11,9 +11,12 @@ db_cursor = db_connection.cursor()
 try:
     db_cursor.execute("SELECT version FROM versionTable;")
     results = db_cursor.fetchone()
-    print("VERSION:", results)
+    print("VERSION:", results[0])
+    db_cursor.execute("SHOW tables;")
+    for row in db_cursor:
+        print("TABLES:", row[0].decode())
     # Check if anything at all is returned         
-except mysql.connector.Error as e:
-    print("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+except mysql.connector.Error as err:
+    print("ERROR %d IN CONNECTION: %s" % (err.args[0], err.args[1]))
 db_cursor.close()
 db_connection.close()
