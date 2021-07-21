47a30eba6a25cbbfd3b6f26fb2a891f86f3cf757
Nick Butler <nwmbutler@gmail.com>
2021-07-21 17:56:43 +0100

    feat: functioning test and supporting files

--- test/dbtest.py
+++ test/dbtest.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
-
+import json
 import mysql.connector
+import filecmp
+
 db_connection = mysql.connector.connect(
   host="mysql_container",
   user="dev",
   passwd="123456",
   database="devopstt"
 )
-db_cursor = db_connection.cursor()
+db_cursor = db_connection.cursor(dictionary=True)
 try:
     db_cursor.execute("SELECT version FROM versionTable;")
-    results = db_cursor.fetchone()
-    print("VERSION:", results[0])
-    db_cursor.execute("SHOW tables;")
-    for row in db_cursor:
-        print("TABLES:", row[0].decode())
-    # Check if anything at all is returned         
+    resultVersion = db_cursor.fetchone()
+    f = open('versionTable.json')
+    version = json.load(f)
+    print("version: ", version)
+    print("resultversion ", resultVersion)
+    if resultVersion == version:
+        print("The Version is updated correctly")
+    else:
+        print("ERROR: The Version is not updated correctly")
+    f = open('tables.json',)
+    content = json.load(f)
+    for i in content['tables']:
+        db_cursor.execute(f"SELECT * FROM {i['name']};")
+        result = db_cursor.fetchone()
+        name = i['name']
+        fileoutput = open("{}.json".format(name))
+        test = json.load(fileoutput)
+        print("db result: ", result)
+        print("Tested against: ", test)
+        if result == test:
+            print("The contents of {} updated correctly".format(name))
+        else:
+           print("ERROR: The contents of {} NOT updated correctly".format(name))
 except mysql.connector.Error as err:
     print("ERROR %d IN CONNECTION: %s" % (err.args[0], err.args[1]))
 db_cursor.close()
